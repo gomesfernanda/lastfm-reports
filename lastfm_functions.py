2cab101f917d31707a6592cc5c63b0e85a9a24b3
Fernanda Gomes <fergomes@gmail.com>
2018-10-14 21:51:43 +0200

    add scrpipt to collect all times tracks

--- lastfm_functions.py
+++ lastfm_functions.py
@@ -2,6 +2,9 @@ import pylast
 import requests
 import json
 import pandas as pd
+import numpy as np
+import time
+from datetime import datetime, timedelta
 
 with open("secrets.json", 'r') as file:
     jsonfile = json.load(file)
@@ -70,8 +73,58 @@ def get_recent_tracks(user, limit=20):
     return recent_tracks_list
 
 
-def get_historical_tracks():
-    return "TBD"
+def get_historical_tracks(user, API_key):
+    todaynow = datetime.now().strftime("%Y%m%d%H%M")
+    pages = get_pages_scrobbled(user, API_key)
+
+    artist_lst = []
+    album_lst = []
+    track_lst = []
+    date_lst = []
+    loved_lst = []
+    mbid_lst = []
+    start_page = 1
+    total_pages = pages
+
+    print("Collecting", (total_pages+1-start_page), "pages of tracks for the user", user)
+    for page in range(start_page, total_pages+1):
+        count = 0
+        time.sleep(0.05)
+        URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+user+"&api_key="+API_key+"&format=json&extended=1&limit=200&page="+str(page)
+        response = s.get(URL).content
+        response_str = response.decode('utf8')
+        response_json = json.loads(response_str)
+        artist_list = response_json['recenttracks']['track']
+        for item in artist_list:
+            count+=1
+            mbid = item['artist']['mbid']
+            artist_name = item['artist']['name']
+            album_name = item['album']['#text']
+            track_name = item['name']
+            loved = item['loved']
+            print(page, artist_name, track_name)
+            mbid_lst.append(mbid)
+            try:
+                date_epoch = item['date']['uts']
+                date_local = time.strftime('%Y-%m-%d %H:%M', time.localtime(int(date_epoch)))
+            except:
+                date_local = "♬ now playing ♬"
+            date_lst.append(date_local)
+            artist_lst.append(artist_name)
+            album_lst.append(album_name)
+            track_lst.append(track_name)
+            loved_lst.append(loved)
+
+            # SAVE PARTIAL FILES EACH 10 PAGES
+
+            if page % 10 == 0:
+                history_tracks = pd.DataFrame(
+                    np.column_stack([date_lst, artist_lst, track_lst, album_lst, loved_lst, mbid_lst]),
+                    columns=['Date Spain', 'Artist', 'Track', 'Album', 'Loved', 'Tags'])
+                history_tracks.to_csv("historical_tracks_" + user + "_" + todaynow + "_partial_" + page + ".csv", sep=',', encoding='utf-8')
+    history_tracks = pd.DataFrame(np.column_stack([date_lst, artist_lst, track_lst, album_lst, loved_lst, mbid_lst]), columns=['Date Spain', 'Date Brazil', 'Artist', 'Track', 'Album', 'Loved', 'Tags'])
+    history_tracks.to_csv("historical_tracks_" + user + "_" + todaynow + ".csv", sep=',', encoding='utf-8')
+    return history_tracks
 
 
 def get_pages_scrobbled(username):
