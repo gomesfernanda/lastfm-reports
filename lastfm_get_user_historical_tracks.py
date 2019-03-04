625ef4e0f1d0d2e4768070c50d0f4ce190ad0491
Fernanda Gomes <fergomes@gmail.com>
2019-03-04 15:29:08 +0100

    export to a specific directory

--- lastfm_get_user_historical_tracks.py
+++ lastfm_get_user_historical_tracks.py
@@ -6,12 +6,13 @@ import numpy as np
 import time
 from datetime import datetime, timedelta
 import argparse
+import os
 
 
 def progbar(curr, total, full_progbar):
     frac = curr/total
     filled_progbar = round(frac*full_progbar)
-    print('\r', '█'*filled_progbar + '-'*(full_progbar-filled_progbar), '[{:>7.2%}]'.format(frac), "[",curr,"|",total,"]", end='')
+    print('\r Progress: |', '█'*filled_progbar + '-'*(full_progbar-filled_progbar), '| [{:>7.2%}]'.format(frac), " [ page",curr,"|",total,"]", end='')
 
 def setup():
     parser = argparse.ArgumentParser()
@@ -38,7 +39,9 @@ def get_pages_scrobbled(username, network):
 def get_historical_tracks(user, API_key, network, session):
     todaynow = datetime.now().strftime("%Y%m%d%H%M")
     scrobbles, pages = get_pages_scrobbled(user, network)
-
+    directory = "export_" + str(user) + "_" + str(todaynow)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     artist_lst = []
     album_lst = []
     track_lst = []
@@ -50,7 +53,6 @@ def get_historical_tracks(user, API_key, network, session):
     print("Collecting", (total_pages + 1 - start_page), "pages of tracks for the user", user)
     for page in range(start_page, total_pages+1):
         progbar(page, total_pages, total_pages // 10)
-        if page % 50 == 0: print("\n========== Saving partials file to disk, page", page ," ==========\n")
         count = 0
         time.sleep(0.05)
         URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+user+"&api_key="+API_key+"&format=json&extended=1&limit=200&page=" + str(page)
@@ -70,18 +72,20 @@ def get_historical_tracks(user, API_key, network, session):
                 date_epoch = item['date']['uts']
                 date_local = time.strftime('%Y-%m-%d %H:%M', time.localtime(int(date_epoch)))
             except:
-                date_local = "♬ now playing ♬"
+                date_local = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             date_lst.append(date_local)
             artist_lst.append(artist_name)
             album_lst.append(album_name)
             track_lst.append(track_name)
             loved_lst.append(loved)
-            # SAVE PARTIAL FILES EACH 50 PAGES
+
+        # SAVE PARTIAL FILES EACH 50 PAGES
         if page % 50 == 0:
+            print("\n\n========== Saving partials file to disk, page", page, "==========\n")
             history_tracks = pd.DataFrame(
                 np.column_stack([date_lst, artist_lst, track_lst, album_lst, loved_lst, mbid_lst]),
                 columns=['Date Spain', 'Artist', 'Track', 'Album', 'Loved', 'Tags'])
-            history_tracks.to_csv("historical_tracks_" + user + "_" + str(todaynow) + "_partial_" + str(page) + ".csv", sep=',', encoding='utf-8')
+            history_tracks.to_csv(directory + "/historical_tracks_" + user + "_" + str(todaynow) + "_partial_" + str(page) + ".csv", sep=',', encoding='utf-8')
     history_tracks = pd.DataFrame(np.column_stack([date_lst, artist_lst, track_lst, album_lst, loved_lst, mbid_lst]), columns=['Date Spain', 'Artist', 'Track', 'Album', 'Loved', 'Tags'])
     history_tracks.to_csv("historical_tracks_" + user + "_" + str(todaynow) + ".csv", sep=',', encoding='utf-8')
     return history_tracks
@@ -91,8 +95,7 @@ def main():
     user = args.user
     API_KEY, API_SECRET, username, password, password_hash = get_credentials()
     s = requests.Session()
-    network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET, username=username,
-                                   password_hash=password_hash)
+    network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET, username=username, password_hash=password_hash)
     try:
         get_historical_tracks(user, API_KEY, network, s)
     except Exception as e:
